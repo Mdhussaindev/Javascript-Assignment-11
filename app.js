b64c4e360883644981b7bcee27acace0a4a39d29
Mdhussaindev <mdhussain.dev00@gmail.com>
2026-01-15 22:35:37 +0500

    update

---
+++ app.js
@@ -0,0 +1,46 @@
+// Q1
+
+function showAlert() {
+  alert("Q1: Link was clicked!");
+}
+
+// Q2
+
+function buyPhone(model) {
+  alert("Thanks for purchasing a phone from us: " + model);
+}
+// Q3
+
+function deleteRow(btn) {
+  var row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
+// Q4
+
+function changeImage(img) {
+  img.src = "nature2.jpg";
+}
+
+function resetImage(img) {
+  img.src = "nature1.jpg";
+}
+
+// Q5
+
+var counterValue = 0;
+
+var counterDisplay = document.getElementById("counterDisplay");
+
+var increaseBtn = document.getElementById("increaseBtn");
+var decreaseBtn = document.getElementById("decreaseBtn");
+
+increaseBtn.addEventListener("click", function() {
+    counterValue++;
+    counterDisplay.innerText = counterValue;
+});
+
+decreaseBtn.addEventListener("click", function() {
+    counterValue--;
+    counterDisplay.innerText = counterValue;
+});
